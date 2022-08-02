c12e5ef0f8489c15d4624cc752c2fc56c359c5d9
RicardoWebProject <luisri_jhn09@outlook.com>
2022-08-02 12:47:54 -0600

    Tipado en typescript

---
+++ Tipado/tipos.js
@@ -0,0 +1,22 @@
+//Variables boolean
+var si = true;
+var no = false;
+//Variables number
+var numero = 96;
+//Variable string
+var nombre = "Ricardo ".concat(numero);
+//Variables array
+var numeros = [1, 2, 3, 4, 5];
+var nombres = ['Ricardo', 'Sara', 'Ne', 'Ja'];
+var coleccion = [9, 8, 7, 6, 5];
+//Variable tupla
+var tupla;
+tupla = [09, 'Ricardo', true];
+console.log(si);
+console.log(numero);
+console.log(numeros);
+console.log(nombre);
+console.log(nombres);
+console.log(coleccion);
+console.log(tupla);
+console.log(tupla[1]);
---
+++ Tipado/tipos.ts
@@ -0,0 +1,27 @@
+//Variables boolean
+let si: boolean = true;
+let no: boolean = false;
+
+//Variables number
+let numero: number = 96;
+
+//Variable string
+let nombre: string = `Ricardo ${numero}`;
+
+//Variables array
+let numeros: number[] = [1,2,3,4,5]
+let nombres: string[] = ['Ricardo', 'Sara', 'Ne', 'Ja']
+let coleccion: Array<number> = [9,8,7,6,5]
+
+//Variable tupla
+let tupla: [number, String, boolean]
+tupla = [09, 'Ricardo', true]
+
+console.log(si);
+console.log(numero);
+console.log(numeros);
+console.log(nombre);
+console.log(nombres);
+console.log(coleccion);
+console.log(tupla);
+console.log(tupla[1]);
